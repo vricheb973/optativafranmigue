32a5136ae5827a3ecfb9b6ee03227a0e8324c400
vricheb973 <vricheb973@g.educaand.es>
2026-01-27 08:53:28 +0100

    Create JwtFilter.java

---
+++ daw-task/src/main/java/com/daw/web/config/JwtFilter.java
@@ -0,0 +1,72 @@
+package com.daw.web.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.daw.services.UsuarioService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String authHeader = request.getHeader("Authorization");
+		String username = null;
+		String token = null;
+		
+		try {
+			if (authHeader != null && authHeader.startsWith("Bearer ")) {
+				token = authHeader.substring(7);
+				username = jwtUtils.extractUsername(token);
+			}
+			
+			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+				UserDetails userDetails = usuarioService.loadUserByUsername(username);
+				if (jwtUtils.validateToken(token, userDetails)) {
+					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+							null, userDetails.getAuthorities());
+					SecurityContextHolder.getContext().setAuthentication(authToken);
+				}
+			}
+			
+			filterChain.doFilter(request, response);
+		} catch (TokenExpiredException e) {
+		    // Token expirado
+		    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		    response.getWriter().write("{\"error\": \"El token ha expirado\"}");
+		} catch (JWTVerificationException e) {
+		    // Token inválido por cualquier otro motivo
+		    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		    response.getWriter().write("{\"error\": \"Token inválido\"}");
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
